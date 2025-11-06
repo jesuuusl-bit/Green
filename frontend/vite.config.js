95cec05f279e020ad0f8e8b82d74c99ff37c8448
jesuuusl-bit <07lopezjesus11@gmail.com>
2025-11-06 07:59:23 -0500

    Integración Sentry en frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  define: {
-    'process.env': process.env,
-  },
-})
+  plugins: [
+    react(),
+    sentryVitePlugin({
+      org: "ucc",
+      project: "green-frontend",
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+      sourcemaps: {
+        assets: "./dist/**",
+      },
+    }),
+  ],
+});
