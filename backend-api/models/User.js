cd693086f33d9e050807a6c6837ff2498c230e78
jesuuusl-bit <07lopezjesus11@gmail.com>
2025-11-08 17:44:53 -0500

    implementacion de Model User.js en b-a(backend-api)

---
+++ backend-api/models/User.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  role: { type: String, enum: ["admin", "operator"], default: "operator" },
+});
+
+export default mongoose.model("User", userSchema);
